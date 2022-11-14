4c7372e2327fda1349e2a7c486c85cdda97ea0ca
Chien Dang <dang.chien@hitachivantara.com>
2022-11-14 22:17:43 +0700

    14/11/2022 Template Engine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
+// HTML loger
 app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('dirname', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
-  res.send('Hello World! dang xuan chien')
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
