d791837534e55c032c4a4531106e3a4e9d482e88
Chien Dang <dang.chien@hitachivantara.com>
2022-12-05 00:17:14 +0700

    04-12-2022 24. Prettier - Code formater

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,20 @@
 const newsRouter = require('./news');
 
-function route(app){
-
+function route(app) {
   app.get('/news', newsRouter);
 
   app.get('/', (req, res) => {
     res.render('home');
-  })
-  
+  });
+
   // Search
   app.get('/search', (req, res) => {
     res.render('search');
-  })
-  
+  });
+
   app.post('/search', (req, res) => {
     res.send('');
-  })
+  });
 }
 
 module.exports = route;
