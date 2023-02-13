bab8cf50bc688c2cd012b9e62cea440242df37aa
Chien Dang <dang.chien@hitachivantara.com>
2023-02-13 23:49:17 +0700

    13-02-2023 29. Create new course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
+const slug = require('mongoose-slug-generator');
 
+mongoose.plugin(slug);
 const Course = new Schema({
-  name: { type: String, default: 'Chien', maxLength: 255 },
-  description: { type: String, maxLength: 255},
-  image: { type: String, maxLength: 255},
-  createAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
+  name: { type: String, requỉe: true, maxLength: 255 },
+  description: { type: String, maxLength: 255 },
+  image: { type: String, maxLength: 255 },
+  slug: { type: String, maxLength: 255 },
+  level: { type: String, maxLength: 255 },
+  videoId: { type: String, maxLength: 255 },
+  slug: { type: String, slug: 'name', unique: true },
+},{
+  timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
